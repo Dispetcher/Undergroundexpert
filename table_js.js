4a136b25d48f07a3ade00751e9efe5f1f4329b88
Dmitry Skoblikov <alligatorsd@gmail.com>
2018-01-27 20:31:18 +0300

    added functions to show_hide all columns

--- table_js.js
+++ table_js.js
@@ -143,5 +143,100 @@ $("#cbxDeep").on("click", function(){
 				$("#metroStats tfoot tr th:nth-child(15)").hide(500);
 		};
 });
-	
+/* Show All columns - Показать все*/
+$("#linkShowAll").on("click",function(){
+				/* Show all cells of table header*/
+				$("#metroStats thead tr th:nth-child(2)").show(500);
+				$("#metroStats thead tr th:nth-child(6)").show(500);
+				$("#metroStats thead tr th:nth-child(7)").show(500);
+				$("#metroStats thead tr th:nth-child(8)").show(500);
+				$("#metroStats thead tr th:nth-child(9)").show(500);
+				$("#metroStats thead tr th:nth-child(11)").show(500);
+				$("#metroStats thead tr th:nth-child(13)").show(500);				
+				$("#metroStats thead tr th:nth-child(15)").show(500);
+
+				/* Show all cells of table body*/	
+				$("#metroStats tbody tr td:nth-child(2)").each(function (){
+					$(this).show(500);
+				});
+				$("#metroStats tbody tr td:nth-child(6)").each(function (){
+					$(this).show(500);
+				});
+				$("#metroStats tbody tr td:nth-child(7)").each(function (){
+					$(this).show(500);
+				});
+				$("#metroStats tbody tr td:nth-child(8)").each(function (){
+					$(this).show(500);
+				});
+				$("#metroStats tbody tr td:nth-child(9)").each(function (){
+					$(this).show(500);
+				});
+				$("#metroStats tbody tr td:nth-child(11)").each(function (){
+					$(this).show(500);
+				});
+				$("#metroStats tbody tr td:nth-child(13)").each(function (){
+					$(this).show(500);
+				});
+				$("#metroStats tbody tr td:nth-child(15)").each(function (){
+					$(this).show(500);
+				});
+
+				/* Show all cells of table footer*/	
+				$("#metroStats tfoot tr th:nth-child(2)").show(500);
+				$("#metroStats tfoot tr th:nth-child(6)").show(500);
+				$("#metroStats tfoot tr th:nth-child(7)").show(500);
+				$("#metroStats tfoot tr th:nth-child(8)").show(500);
+				$("#metroStats tfoot tr th:nth-child(9)").show(500);
+				$("#metroStats tfoot tr th:nth-child(11)").show(500);
+				$("#metroStats tfoot tr th:nth-child(13)").show(500);
+				$("#metroStats tfoot tr th:nth-child(15)").show(500);
+});
+/* Hide All columns - Скрыть все*/	
+$("#linkHideAll").on("click",function(){
+				/* Hide all cells of table header*/
+				$("#metroStats thead tr th:nth-child(2)").hide(500);
+				$("#metroStats thead tr th:nth-child(6)").hide(500);
+				$("#metroStats thead tr th:nth-child(7)").hide(500);
+				$("#metroStats thead tr th:nth-child(8)").hide(500);
+				$("#metroStats thead tr th:nth-child(9)").hide(500);
+				$("#metroStats thead tr th:nth-child(11)").hide(500);
+				$("#metroStats thead tr th:nth-child(13)").hide(500);				
+				$("#metroStats thead tr th:nth-child(15)").hide(500);
+
+				/* Hide all cells of table body*/	
+				$("#metroStats tbody tr td:nth-child(2)").each(function (){
+					$(this).hide(500);
+				});
+				$("#metroStats tbody tr td:nth-child(6)").each(function (){
+					$(this).hide(500);
+				});
+				$("#metroStats tbody tr td:nth-child(7)").each(function (){
+					$(this).hide(500);
+				});
+				$("#metroStats tbody tr td:nth-child(8)").each(function (){
+					$(this).hide(500);
+				});
+				$("#metroStats tbody tr td:nth-child(9)").each(function (){
+					$(this).hide(500);
+				});
+				$("#metroStats tbody tr td:nth-child(11)").each(function (){
+					$(this).hide(500);
+				});
+				$("#metroStats tbody tr td:nth-child(13)").each(function (){
+					$(this).hide(500);
+				});
+				$("#metroStats tbody tr td:nth-child(15)").each(function (){
+					$(this).hide(500);
+				});
+
+				/* Hide all cells of table footer*/	
+				$("#metroStats tfoot tr th:nth-child(2)").hide(500);
+				$("#metroStats tfoot tr th:nth-child(6)").hide(500);
+				$("#metroStats tfoot tr th:nth-child(7)").hide(500);
+				$("#metroStats tfoot tr th:nth-child(8)").hide(500);
+				$("#metroStats tfoot tr th:nth-child(9)").hide(500);
+				$("#metroStats tfoot tr th:nth-child(11)").hide(500);
+				$("#metroStats tfoot tr th:nth-child(13)").hide(500);
+				$("#metroStats tfoot tr th:nth-child(15)").hide(500);
+});	
 })(jQuery);
