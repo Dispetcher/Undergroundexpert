b5ef5d277a1cad82743102b914fddd6c48a6a5d4
Dmitry Skoblikov <alligatorsd@gmail.com>
2018-02-21 21:47:57 +0300

    final up 21/02 for mobile manage_sub

--- manage_sub.js
+++ manage_sub.js
@@ -4,7 +4,26 @@
 		$('.mailpoet_paragraph:eq(0)').css('display', 'none');
 		$('.mailpoet_paragraph:eq(4)').css('display', 'none');
 		$('.mailpoet_paragraph:eq(5)').css('text-align', 'center');
-		$('.mailpoet_text:eq(0)').css('margin-left','35px');
+		$('.mailpoet_text:eq(0)').css('margin-left','34px');
 		$('.mailpoet_text').removeAttr('disabled');
+		
+		//Change selector
+		$('.mailpoet_select option[value=subscribed]').html('Подписка оформлена');
+		$('.mailpoet_select option[value=unsubscribed]').html('Подписка отменена');
 	 });
+	$(document).ready(mmedia);
+	$(window).resize(mmedia);
+	
+	function mmedia(){
+			if (window.matchMedia('(max-width: 445px)').matches) {
+				$('.mailpoet_text:eq(0)').css('margin-left','24px');
+				$('.mailpoet_text:eq(1)').css('margin-left','-10px');
+			}else if(window.matchMedia('(max-width: 374px)').matches){
+				$('.mailpoet_text:eq(0)').css('margin-left','24px');
+				$('.mailpoet_text:eq(1)').css('margin-left','-10px');
+			}else{
+				$('.mailpoet_text:eq(0)').css('margin-left','34px');
+				$('.mailpoet_text:eq(1)').removeAttr('style');
+			}
+	}
 })(jQuery);
