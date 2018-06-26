46650b2994e9e73b67617ea34997601a913c56b4
Dmitry Skoblikov <alligatorsd@gmail.com>
2018-06-26 14:09:24 +0400

    yandex meta tag

--- header.php
+++ header.php
@@ -1,35 +1,38 @@
 <?php
+/*
+Template Name: Header - Заголовок
+*/
 $cl = pll_current_language();
 if ($cl == 'ru') {
     $logo = 'logo';
     $second_logo = 'second_logo';
     $descriptor = 'descriptor';
     $link_to_index = "link_to_index";
-    $subscribeButton = "subscribe_button";
-    $subscribeButtonLink = "subscribe_button_link";
+	$subscribeButton = "subscribe_button";
+	$subscribeButtonLink = "subscribe_button_link";
     $search_field_text = "search_field_text";
     $search_dropdown_text = "search_dropdown_text";
     $title = "seo_title";
     $description = "seo_description";
     $prefix_title = ' - новости строительства и развития подземных сооружений';
     $prefix_description = ' - статьи о строительстве метро, поземных переходов, автомобильных развязок.';
-    $urlForSearch = get_bloginfo('siteurl');
+	$urlForSearch = get_bloginfo('siteurl');
 } else {
     $logo = 'logo-eng';
     $second_logo = 'second_logo-eng';
     $descriptor = 'descriptor-eng';
     $link_to_index = "link_to_index-eng";
-    $subscribeButton = "subscribe_button-eng";
-    $subscribeButtonLink = "subscribe_button_link-eng";
+	$subscribeButton = "subscribe_button-eng";
+	$subscribeButtonLink = "subscribe_button_link-eng";
     $search_field_text = "search_field_text-eng";
     $search_dropdown_text = "search_dropdown_text-eng";
     $title = "seo_title-eng";
     $description = "seo_description-eng";
     $prefix_title = ' - news construction and development pose structures';
     $prefix_description = ' - articles about the construction of the subway, underground crossings, road junctions.';
-    $urlForSearch = get_bloginfo('siteurl') . "/";
+	$urlForSearch = get_bloginfo('siteurl') . "/";
 }
-    $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );  
+	$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );  
 ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
@@ -37,29 +40,77 @@ if ($cl == 'ru') {
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="<?=((substr($_SERVER['REQUEST_URI'], 0, 4) === '/en/') ? 'en-us' : 'ru')?>"> <!--<![endif]-->
     <head>
-    <?php foreach(pll_the_languages(array('raw' => 1, 'hide_if_no_translation' => 1)) as $lang): ?>
-        <link rel="alternate" hreflang="<?=$lang['slug']?>" href="<?=$lang['url']?>" />
-    <?php endforeach; ?>
+	<?php foreach(pll_the_languages(array('raw' => 1, 'hide_if_no_translation' => 1)) as $lang): ?>
+		<link rel="alternate" hreflang="<?=$lang['slug']?>" href="<?=$lang['url']?>" />
+	
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name = "twitter:card"  content = "summary"  /> 
-        <meta name = "twitter:site"  content = "UndergroundExpert"  />
-        <meta name = "twitter:title"  content = "<?php if(get_field($title, 'category_'.$cat)): the_field($title, 'category_'.$cat ); else: wp_title(''); echo $prefix_title; endif; ?>" /> 
-        <meta name = "twitter:description "  content = "<?php echo the_excerpt();?>" /> 
-        <meta name = "twitter:image" content="<?php echo get_the_post_thumbnail_url(); ?>" />
-        <meta name = "twitter:image:width" content="295"/>
-        <meta name = "twitter:image:height" content="195"/>
-        <meta name = "twitter:domain"  content = "http://undergdroundexpert.info"  /> 
-        <link href="/favicon.ico" rel="icon" type="image/vnd.microsoft.icon" />
+		
+		<!-- Facebook meta -->
+		<meta property="og:url"           content="<?=$lang['url']?>" />
+		
+	<?php endforeach; ?>
+		
+  		<meta property="og:type"          content="website" />
+ 		<meta property="og:title"         content="<?php if(get_field($title, 'category_'.$cat)): the_field($title, 'category_'.$cat ); else: wp_title(''); echo $prefix_title; endif; ?>" />
+		
+		<?if(is_category()): ?>
+			<?php if(get_field($description)):?>
+                <meta property="og:description" content="<?php the_field($description, 'category_'.$cat)?>">
+            <?php else:?>
+                <meta property="og:description" content="<? single_cat_title(); echo $prefix_description;?>">
+            <?php endif;?>
+			
+		<?else:?>
+			<?php if(get_field($description)):?>
+				<meta property="og:description" content="<?php the_field($description)?>">
+			<?php else:?>
+				<meta property="og:description" content="<?php wp_title(''); echo $prefix_description;?>">
+			<?php endif;?>
+		<?endif;?>
+		
+  		<meta property="og:image"         content="<?php echo get_the_post_thumbnail_url(); ?>" />
+	
+		
+		<!-- Twitter card meta -->
+		<meta name = "twitter:card"  content = "summary"  /> 
+		<meta name = "twitter:site"  content = "UndergroundExpert"  />
+		<meta name = "twitter:title"  content = "<?php if(get_field($title, 'category_'.$cat)): the_field($title, 'category_'.$cat ); else: wp_title(''); echo $prefix_title; endif; ?>" /> 
+		
+		<?if(is_category()): ?>
+
+			<?php if(get_field($description)):?>
+                <meta name="twitter:description " content="<?php the_field($description, 'category_'.$cat)?>">
+            <?php else:?>
+                <meta name="twitter:description " content="<? single_cat_title(); echo $prefix_description;?>">
+            <?php endif;?>
+			
+		<?else:?>
+
+			<?php if(get_field($description)):?>
+				<meta name="twitter:description" content="<?php the_field($description)?>">
+			<?php else:?>
+				<meta name="twitter:description" content="<?php wp_title(''); echo $prefix_description;?>">
+			<?php endif;?>
+
+		<?endif;?>
+
+		<meta name = "twitter:image" content="<?php echo get_the_post_thumbnail_url(); ?>" />
+		<meta name = "twitter:image:width" content="295"/>
+		<meta name = "twitter:image:height" content="195"/>
+		<meta name = "twitter:domain"  content = "http://undergdroundexpert.info"  /> 
+		<!-- Yandex meta -->
+		<meta name="yandex-verification" content="aedee3c950d6e13c" />
+		
+		<link href="/favicon.ico" rel="icon" type="image/vnd.microsoft.icon" />
         <?php
         if ( get_option('blog_public') !== '0' ) {
             ?><meta name='robots' content='all' /><?php
         }
 
-            if(is_category()): 
-        ?>
-        <link href="/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
-            <title><?php if(get_field($title, 'category_'.$cat)): the_field($title, 'category_'.$cat ); else: single_cat_title(); echo $prefix_title;  endif;?></title>
+			if(is_category()): 
+		?>
+			<title><?php if(get_field($title, 'category_'.$cat)): the_field($title, 'category_'.$cat ); else: single_cat_title(); echo $prefix_title;  endif;?></title>
 
             <?php if(get_field($description)):?>
                 <meta name="description" content="<?php the_field($description, 'category_'.$cat)?>">
@@ -67,57 +118,70 @@ if ($cl == 'ru') {
                 <meta name="description" content="<? single_cat_title(); echo $prefix_description;?>">
             <?php endif;?>
 
-            <meta name="keywords" content="<?php the_field('seo_keywords', 'category_'.$cat)?>">
-            
+			<meta name="keywords" content="<?php the_field('seo_keywords', 'category_'.$cat)?>">
+
         <?php else:?>
             
-            <?php if(get_field($title)):?>
-                <title><?php the_field($title)?></title>
-            <?php else:?>
-                <!--<title><?php wp_title('');?></title>-->
+			<?php if(get_field($title)):?>
+				<title><?php the_field($title)?></title>
+			<?php else:?>
+				<!--<title><?php wp_title('');?></title>-->
                 <title><?php wp_title(''); ?><?php echo $prefix_title?></title>
-            <?php endif;?>
+			<?php endif;?>
             
-            <?php if(get_field($description)):?>
-                <meta name="description" content="<?php the_field($description)?>">
-            <?php else:?>
-                <meta name="description" content="<?php wp_title(''); echo $prefix_description;?>">
-            <?php endif;?>
-            <?php if(get_field('seo_keywords')):?>
-                <meta name="keywords" content="<?php the_field('seo_keywords')?>">
-            <?php endif;?>
-        <?php endif;?>
+			<?php if(get_field($description)):?>
+				<meta name="description" content="<?php the_field($description)?>">
+			<?php else:?>
+				<meta name="description" content="<?php wp_title(''); echo $prefix_description;?>">
+			<?php endif;?>
+			<?php if(get_field('seo_keywords')):?>
+				<meta name="keywords" content="<?php the_field('seo_keywords')?>">
+			<?php endif;?>
+		<?php endif;?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-                
+		
+		<!-- Browser lang -->
+		<script src="<?php bloginfo('template_url')?>/js/browser_lang.js"></script>	
+					 
         <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/normalize.min.css">
         <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/main_new.css">
         <link rel="stylesheet" href="<?php bloginfo('template_url')?>/js/owlcarousel/assets/owl.carousel.min.css">
-        <link rel="stylesheet" href="<?php bloginfo('template_url')?>/js/fancybox/jquery.fancybox.css">
-                
-        <?php if(get_the_ID() == '10077'){?>
-        <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/manage_sub.css">
-        <?php };?>
-        
-        <script src="<?php bloginfo('template_url')?>/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-        <?php wp_head();?>
+		<link rel="stylesheet" href="<?php bloginfo('template_url')?>/js/fancybox/jquery.fancybox.css">
+				
+		<?php if(get_the_ID() == '10077'){?>
+		<link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/manage_sub.css">
+		<?php };?>
+		
+		<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+		<script>
+ 			var OneSignal = window.OneSignal || [];
+  			OneSignal.push(function() {
+   				OneSignal.init({
+     				appId: "b8c31b30-bde6-431e-91be-85cb186c2824",
+   		 		});
+ 			 });
+		</script>
+		
+		<script src="<?php bloginfo('template_url')?>/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+		<?php wp_head();?>
     </head>
     <?php
-        $dop_classes = '';
-        if(!is_front_page()) $dop_classes = 'inner_page';
-    ?>
-    <body <?php body_class($dop_classes)?>>
+		$dop_classes = '';
+		if(!is_front_page()) $dop_classes = 'inner_page';
+	?>
+	<body <?php body_class($dop_classes)?>>
         <!--[if lt IE 8]>
             <p class="browserupgrade">Вы используете <strong>устаревший</strong> браузер. Пожалуйста <a href="http://browsehappy.com/">обновите браузер</a> для полноценной работы сайта.</p>
         <![endif]-->
-        <!--LiveInternet counter-->
-        <script type="text/javascript">
-    new Image().src = "//counter.yadro.ru/hit?r"+
-    escape(document.referrer)+((typeof(screen)=="undefined")?"":
-    ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
-    screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
-    ";h"+escape(document.title.substring(0,150))+
-    ";"+Math.random();</script>
-        <!--/LiveInternet-->
+		<!--LiveInternet counter-->
+		<script type="text/javascript">
+	new Image().src = "//counter.yadro.ru/hit?r"+
+	escape(document.referrer)+((typeof(screen)=="undefined")?"":
+	";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+	screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+	";h"+escape(document.title.substring(0,150))+
+	";"+Math.random();</script>
+		<!--/LiveInternet-->
 
         <span class="mobile_menu_opener"></span>
         <div class="mobile_menu">
@@ -144,38 +208,55 @@ if ($cl == 'ru') {
                         </div><!--/.logo-->
                         <div class="mob_sep"></div>
                         <div class="dtc <?php if(is_front_page()): echo 'search_wrap front_search'; else: echo 'search_wrap'; endif; ?>">
-                            <form method="get" name="searchform" id="searchform"  action="<?=$urlForSearch?>">
-                                <div class="dt">
-                                    <div class="dtc search_field">
-                                        <input type="text" name="s" id="s" value="" placeholder="<?php the_field($search_field_text, 'options')?>"/>
-                                        <span class="search_date_notice"><?php the_field($search_dropdown_text, 'options')?></span>
-                                    </div>
-                                    <div class="dtc">
-                                        <input id="btnSearch" type="submit" name="submit" value="Поиск" />
-                                    </div>
-                                </div>
-                            </form>
+							<form method="get" name="searchform" id="searchform" action="<?=$urlForSearch?>">
+								<div class="dt">
+									<div class="dtc search_field">
+										<input type="text" name="s" id="s" value="" placeholder="<?php the_field($search_field_text, 'options')?>"/>
+										<span class="search_date_notice"><?php the_field($search_dropdown_text, 'options')?></span>
+									</div>
+									<div class="dtc">
+										<input id="btnSearch" type="submit" name="submit" value="Поиск" />
+									</div>
+								</div>
+							</form>
+
                         </div>
-                        <?php
-                            if(!is_front_page()):
-                        ?>
-                            <div class="dtc top_subscribe_wrap">
-                                <a class="button" href="<?php the_field($subscribeButtonLink, 'options');?>"><?php the_field($subscribeButton, 'options');?></a>
-                            </div>
-                        <?php endif;?>
-                        <div class="lsw-wrap">
-                            <ul><?php 
-                                pll_the_languages(array(
-                                    'show_flags' => 1,
-                                    'show_names' => 0,
-                                    'dropwdown' => 0)
-                                );?>
-                            </ul>
-                        </div>
+						<?php
+							if(!is_front_page()):
+						?>
+							<div class="dtc top_subscribe_wrap">
+								<a class="button" href="<?php the_field($subscribeButtonLink, 'options');?>"><?php the_field($subscribeButton, 'options');?></a>
+							</div>
+						<?php endif;?>
+						<div class="lsw-wrap">
+							<ul><?php 
+								pll_the_languages(array(
+									'show_flags' => 1,
+									'show_names' => 0,
+									'dropwdown' => 0)
+								);?>
+							</ul>
+						</div>
                     </div><!--/.dt-->
                 </div><!--/.center_wrap-->
             </div><!--/#top-->
         </header><!--/#header-->
+		
+		<?php if( is_front_page() ): ?>
+		<div id="person_data">
+			<div class="center_wrap">
+				<div class="dt person_data_center">
+					<?php if ($cl==ru): ?>
+					<span>Данный сайт <a href="http://undergroundexpert.info/soglasie-na-obrabotku-personalnyh-dannyh/">использует файлы «cookies»</a> с целью персонализации сервисов и повышения удобства пользования веб-сайтом.<br>«Cookies» представляют собой небольшие файлы, содержащие информацию о предыдущих посещениях веб-сайта.<br>Если вы не хотите использовать файлы «cookies», измените настройки браузера.</span>
+					<?php ;else: ?>
+					<span>This website uses <a href="http://undergroundexpert.info/en/consent-to-the-processing-of-personal-data/">files called cookies</a> to customise your experience and make the website more convenient to use. <br>Cookies are small files that contain information about previous visits to websites.<br>If you want to stop accepting cookies, please change your browser settings.</span>
+					<?php ;endif; ?>
+					<a class="close_person_data"></a>
+				</div>
+			</div>	
+		</div> <!--/#person_data-->
+		<?php ;endif; ?>
+		
         <div id="main_nav">
             <div class="center_wrap">
                 <nav class="main_nav">
